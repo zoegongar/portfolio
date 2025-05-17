6c304d2431602947aeaf1fd081b92545caa29290
zoegongar <zoegonzalezgarcia@gmail.com>
2025-05-17 13:16:18 +0200

    cada carta en su sitio

--- main/js/contact.js
+++ main/js/contact.js
@@ -4,28 +4,33 @@ export function renderContactSection() {
   console.log("estoy en contact");
     mainContent.innerHTML = `
       <div class="contact-deck">
-        <div class="playing-card interactive-card">
-          <h2 id="linkedIn" class="text-playing-cards" data-i18n="contact.linkedIn">LinkedIn</h2>
+       <div class="upper-cards">
+        <div class="playing-card interactive-card" >
+          <div id="aboutMe" class="text-playing-cards">LinkedIn</div>
         </div>
-        <div class="playing-card interactive-card" id="github">
-          <h2 class="text-playing-cards" data-i18n="contact.github">GitHub</h2>
+        <div class="playing-card interactive-card" >
+          <h2 id="contact" class="text-playing-cards">GitHub</h2>
         </div>
-        <div class="playing-card interactive-card" id="email">
-          <h2 class="text-playing-cards" data-i18n="contact.email">Email</h2>
+        <div class="playing-card interactive-card" >
+          <h2 id="cv" class="text-playing-cards">E-mail</h2>
         </div>
-        <div class="middle-cards">
+      </div>
+      <div class="middle-cards">
         <div class="playing-card naipe-card" >
           <h2 id="playing-card-back" class="text-playing-cards">Contact</h2>
         </div>
-        <div class="playing-card interactive-card" id="settings">
-          <h2 class="text-playing-cards" data-i18n="contact.settings">Settings</h2>
+        <div class="playing-card location-indicator">
+          <h2 id="settings" class="text-playing-cards">Settings</h2>
         </div>
-        <div class="playing-card interactive-card" id="backToMain">
-          <h2 class="text-playing-cards" data-i18n="contact.backToMain">Back to Main</h2>
+      </div>
+      <div class="bottom-cards">
+        <div class="playing-card naipe-card">
+          <h2 id="tutorials" class="text-playing-cards">Back to main</h2>
         </div>
-        <div class="playing-card interactive-card" id="exit">
-          <h2 class="text-playing-cards" data-i18n="contact.exit">Exit</h2>
+        <div class="playing-card naipe-card">
+          <h2 id="projects" class="text-playing-cards">Exit</h2>
         </div>
+      </div>
       </div>
     `;
   }
